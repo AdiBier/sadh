a60cf0ca35292f0f07c8a0cb920cbd5f30c7fa1b
adrian.bierkat <adrian.bierkat@mayeryn.com>
2020-03-11 21:22:41 +0100

    Initial commit

---
+++ src/test/java/com/ab/sadh/SadhApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ab.sadh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SadhApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
